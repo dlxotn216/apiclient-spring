ca95301a64a12cefdcb611dda130be07f6117023
taesu <taesu@crscube.co.kr>
2024-05-24 23:03:44 +0900

    Initial config

---
+++ src/main/kotlin/io/taesu/apiclientspring/ApiclientSpringApplication.kt
@@ -0,0 +1,11 @@
+package io.taesu.apiclientspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiclientSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiclientSpringApplication>(*args)
+}
---
+++ src/test/kotlin/io/taesu/apiclientspring/ApiclientSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.taesu.apiclientspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiclientSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
