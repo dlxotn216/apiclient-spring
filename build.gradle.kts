58179d3bc6c2411da3621515d48b0b2c3b9f89d5
taesu <taesu@crscube.co.kr>
2024-05-25 21:00:02 +0900

    reactor step verifier 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    testImplementation("io.projectreactor:reactor-test:3.6.5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
