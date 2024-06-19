aeb08314aa7530a23c29f53ade2d1885c9b55dbc
taesu <taesu@crscube.co.kr>
2024-06-19 19:30:03 +0900

    config
    set default timezone

--- src/main/kotlin/io/taesu/apiclientspring/ApiclientSpringApplication.kt
+++ src/main/kotlin/io/taesu/apiclientspring/ApiclientSpringApplication.kt
@@ -1,11 +1,20 @@
 package io.taesu.apiclientspring
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.time.ZoneId
+import java.util.*
 
 @SpringBootApplication
-class ApiclientSpringApplication
+class ApiclientSpringApplication {
+    @PostConstruct
+    fun onConstruct() {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("UTC")))
+    }
+}
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("UTC")))
     runApplication<ApiclientSpringApplication>(*args)
 }
